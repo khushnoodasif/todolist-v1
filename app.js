495ec643e26a72ecc799a521670641f06e40846e
Khushnood Asif <khushnood99@gmail.com>
2021-11-16 00:30:04 +0000

    added new items

--- app.js
+++ app.js
@@ -1,49 +1,39 @@
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const app = express();
 
+var items = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = "Monday";
-      break;
-    case 1:
-      day = "Tuesday";
-      break;
-    case 2:
-      day = "Wednesday";
-      break;
-    case 3:
-      day = "Thursday";
-      break;
-    case 4:
-      day = "Friday";
-      break;
-    case 5:
-      day = "Saturday";
-      break;
-    case 6:
-      day = "Sunday";
-      break;
-      console.log("Error! =" + currentDay);
-  }
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
 });
 
+app.post('/', function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect('/');
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
